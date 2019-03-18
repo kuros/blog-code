37f9aeac39d2dbf3a647bc139a8516836c0563be
Kumar Rohit <incrediblevicky@gmail.com>
2019-03-18 23:03:28 +0530

    React - header

--- react/first-app/src/components/home/HomePage.tsx
+++ react/first-app/src/components/home/HomePage.tsx
@@ -10,20 +10,6 @@ class HomePage extends Component {
                     <p>Welcome to your first React App</p>
                     <Link to='about'  className="btn btn-primary btn-lg" >More</Link>
                 </div>
-
-                <div>
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="email">Email address:</label>
-                            <input type="email" className="form-control" id="email"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="pwd">Password:</label>
-                            <input type="password" className="form-control" id="pwd"/>
-                        </div>
-                        <button type="submit" className="btn btn-default btn-primary">Submit</button>
-                    </form>
-                </div>
             </div>
         );
     }
