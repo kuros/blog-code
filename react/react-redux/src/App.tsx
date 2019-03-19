66a6fadffacd3cff1ee090164ad3f9e3a13305df
Kumar Rohit <incrediblevicky@gmail.com>
2019-03-19 20:05:32 +0530

    initial commit

--- react/react-redux/src/App.tsx
+++ react/react-redux/src/App.tsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import HomePage from "./components/home/HomePage";
+import {BrowserRouter, Route} from "react-router-dom";
+import AboutPage from "./components/about/AboutPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/common/Header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='container'>
+                <BrowserRouter>
+                    <Header/>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/about' component={AboutPage}/>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ react/react-redux/src/components/about/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React, {FunctionComponent} from "react";
+
+
+const AboutPage: FunctionComponent = () => {
+    return (
+        <div>
+            <div className='jumbotron'>
+                This is the about Page
+            </div>
+        </div>
+    )
+};
+
+export default AboutPage;
