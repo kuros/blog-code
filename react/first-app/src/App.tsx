3b26274507c4b64e53b31cc8b1cc2cf07558c752
Kumar Rohit <incrediblevicky@gmail.com>
2019-03-23 00:50:30 +0530

    React - switch

--- react/first-app/src/App.tsx
+++ react/first-app/src/App.tsx
@@ -1,20 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 import HomePage from "./components/home/HomePage";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import AboutPage from "./components/about/AboutPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/common/Header";
+import NotFoundPage from "./NotFoundPage";
 
 class App extends Component {
     render() {
         return (
             <div className='container'>
-                <BrowserRouter>
-                    <Header/>
+                <Header/>
+                <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/about' component={AboutPage}/>
-                </BrowserRouter>
+                    <Route component={NotFoundPage}/>
+                </Switch>
             </div>
         );
     }
---
+++ react/first-app/src/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+import React, {FunctionComponent} from "react";
+
+const NotFoundPage:FunctionComponent = () => <div>Oops Page Not found.</div>;
+
+export default NotFoundPage;
