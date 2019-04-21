b17fdf092325b3a630aec6db988493592efd6fab
Kumar Rohit <incrediblevicky@gmail.com>
2019-04-21 23:20:57 +0530

    initial commit

---
+++ spring/spring-security-basics/src/main/java/in/kuros/blog/code/spring/security/basics/SpringSecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package in.kuros.blog.code.spring.security.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicsApplication.class, args);
+    }
+
+}
---
+++ spring/spring-security-basics/src/test/java/in/kuros/blog/code/spring/security/basics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,16 @@
+package in.kuros.blog.code.spring.security.basics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityBasicsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
