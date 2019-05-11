eed99f916e174a03b9b6ffff983ea5fda95316c9
Kumar Rohit <incrediblevicky@gmail.com>
2019-05-12 00:41:07 +0530

    authentication provider - initial commit

---
+++ spring/spring-security-authentication-provider/src/main/java/in/kuros/blog/code/spring/security/authentication/provider/SpringSecurityAuthenticationProviderApplication.java
@@ -0,0 +1,13 @@
+package in.kuros.blog.code.spring.security.authentication.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationProviderApplication.class, args);
+    }
+
+}
---
+++ spring/spring-security-authentication-provider/src/test/java/in/kuros/blog/code/spring/security/authentication/provider/SpringSecurityAuthenticationProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package in.kuros.blog.code.spring.security.authentication.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityAuthenticationProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
