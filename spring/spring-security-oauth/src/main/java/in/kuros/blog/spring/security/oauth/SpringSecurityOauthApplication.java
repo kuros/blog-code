0bfe0932c3f2b78afd6ce039e60d524da1b4f637
Kumar Rohit <incrediblevicky@gmail.com>
2019-09-01 17:19:33 +0530

    spring-oauth: initial commit

---
+++ spring/spring-security-oauth/src/main/java/in/kuros/blog/spring/security/oauth/SpringSecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package in.kuros.blog.spring.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthApplication.class, args);
+	}
+
+}
