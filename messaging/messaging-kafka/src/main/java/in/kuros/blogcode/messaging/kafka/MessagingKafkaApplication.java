3a7a8a577f2106e969851935822942ef77686c40
Kumar Rohit <incrediblevicky@gmail.com>
2020-05-03 00:15:27 +0530

    initial commit

---
+++ messaging/messaging-kafka/src/main/java/in/kuros/blogcode/messaging/kafka/MessagingKafkaApplication.java
@@ -0,0 +1,13 @@
+package in.kuros.blogcode.messaging.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingKafkaApplication.class, args);
+    }
+
+}
---
+++ messaging/messaging-kafka/src/test/java/in/kuros/blogcode/messaging/kafka/MessagingKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kuros.blogcode.messaging.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
